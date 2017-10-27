30b8eaa3cd77f39752b3806238f2738cf69b04ea
k33g <ph.charriere@gmail.com>
2017-10-27 08:59:41 +0200

    blend update

--- runall-bis.sh
+++ runall-bis.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 
-startPort=9090
+startPort=6060
 
-for i in `seq 11 20`;
+for i in `seq 1 6`;
 do
     resultat=$(($startPort+$i))
     REDIS_RECORDS_KEY="bsg-the-plan" SERVICE_NAME="R" SERVICE_PORT=$resultat PORT=$resultat java  -jar target/k-raider-1.0-SNAPSHOT-fat.jar&
